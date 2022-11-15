b89ed1d53c55c9b01d3702da83c7e0e836970e44
Artem <114680782+tmr1n@users.noreply.github.com>
2022-11-15 18:32:08 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-let div = document.getElementById('item')
-let items = document.getElementById('items')
-
-document.querySelector("#inpButton").onclick = function(){
-    if(document.querySelector("#inp") !== null){
-    items.append(div)
-    }
-}
